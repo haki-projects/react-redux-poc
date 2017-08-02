91484a91d2c1263291e809cce23d5b73011d53e7
Robbie Robinson <haki.projects@gmail.com>
2017-08-02 21:29:53 +0200

    introduced state into event handler
    Introduced the this.setState function into the input event handler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyCj_QgiZWhHaU_WKRST2N7V1lQOSkZZ0l0';
 // Create a new component. this component should produce some HTML
 // Take this component's generated HTML and put it on the page (in the DOM)
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 ReactDOM.render(<App />, document.querySelector('.container'));
