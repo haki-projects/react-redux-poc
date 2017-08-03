770c6e2d2a0b834d1829ba255b9033b8dcfa7d35
haki-projects <30647767+haki-projects@users.noreply.github.com>
2017-08-03 16:48:33 +0200

    Added markup for youtube API data
    Added markup for the youtube API data.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,12 +5,14 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: ' ' };
+    this.state = { term: '' };
   }
   render() {
     return (
   <div>
-    <input onChange={event => this.setState({ term: event.target.value })} />
+    <input
+    value={this.state.term}
+    onChange={event => this.setState({ term: event.target.value })} />
   </div>
     );
   }
